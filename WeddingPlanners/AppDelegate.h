72edbded82dc80f7725dbfb65f5ca9984753d932
Rohan Sawhney <rohansawhney@dyn-160-39-236-96.dyn.columbia.edu>
2014-03-08 14:33:45 -0500

    Initial Commit

---
+++ WeddingPlanners/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeddingPlanners
+//
+//  Created by Rohan Sawhney on 3/8/14.
+//  Copyright (c) 2014 DSTEWeddingPlanners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeddingPlanners/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeddingPlanners
+//
+//  Created by Rohan Sawhney on 3/8/14.
+//  Copyright (c) 2014 DSTEWeddingPlanners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
